cfe5c7e0a66f08acb3c73b06f663e5d4fe6435d6
Hetarth Chopra <34271010+choprahetarth@users.noreply.github.com>
2019-12-28 23:14:10 +0530

    changed the previous file

--- ArduinoPC2.ino
+++ ArduinoPC2.ino
@@ -1,203 +1,88 @@
-#include <Servo.h>
+#define ledIndicator 13
+#include "SparkFun_AS7265X.h" 
+AS7265X sensor;
+#include <Wire.h>
 
-Servo myServo;
-byte servoPin = 8;
-byte servoMin = 10;
-byte servoMax = 170;
-byte servoPos = 0;
-byte newServoPos = servoMin;
-
-const byte numLEDs = 2;
-byte ledPin[numLEDs] = {12, 13};
-unsigned long LEDinterval[numLEDs] = {200, 400};
-unsigned long prevLEDmillis[numLEDs] = {0, 0};
-
-const byte buffSize = 40;
-char inputBuffer[buffSize];
-const char startMarker = '<';
-const char endMarker = '>';
-byte bytesRecvd = 0;
-boolean readInProgress = false;
-boolean newDataFromPC = false;
-
-char messageFromPC[buffSize] = {0};
-int newFlashInterval = 0;
-float servoFraction = 0.0; // fraction of servo range to move
-
-
-unsigned long curMillis;
-
-unsigned long prevReplyToPCmillis = 0;
-unsigned long replyToPCinterval = 1000;
-
-//=============
+String inputVariable;
+int readFlag = 5;
 
 void setup() {
   Serial.begin(9600);
-  
-    // flash LEDs so we know we are alive
-  for (byte n = 0; n < numLEDs; n++) {
-     pinMode(ledPin[n], OUTPUT);
-     digitalWrite(ledPin[n], HIGH);
-  }
-  delay(500); // delay() is OK in setup as it only happens once
-  
-  for (byte n = 0; n < numLEDs; n++) {
-     digitalWrite(ledPin[n], LOW);
+  pinMode(ledIndicator, OUTPUT);
+  if(sensor.begin() == false)
+  {
+    Serial.println("Sensor does not appear to be connected. Please check wiring. Freezing...");
+    while(1);
   }
   
-    // initialize the servo
-  myServo.attach(servoPin);
-  moveServo();
-  
-    // tell the PC we are ready
-  Serial.println("<Arduino is ready>");
+  Serial.println("A,B,C,D,E,F,G,H,I,J,K,L,R,S,T,U,V,W");
 }
 
-//=============
-
-void loop() {
-  curMillis = millis();
-  getDataFromPC();
-  updateFlashInterval();
-  updateServoPos();
-  replyToPC();
-  flashLEDs();
-  moveServo();
+void initiator(int a){
+  if (a == 1 && initiationFlag == 1){
+  inputVariable = Serial.readString();
+  if (inputVariable == 'm') {
+    digitalWrite(ledIndicator, HIGH);
+  }
+  delay(100);
+  Serial.println("XCommunication Initiated"); // write a string
+  initiationFlag = 0;
+  }
+  else if (a == 0 && finalizationFlag == 0){
+   Serial.println("Communication Stopped");
+   digitalWrite(ledIndicator, LOW);
+   sensor.disableIndicator();
+    }
 }
 
-//=============
-
-void getDataFromPC() {
-
-    // receive data from PC and save it into inputBuffer
-    
-  if(Serial.available() > 0) {
-
-    char x = Serial.read();
-
-      // the order of these IF clauses is significant
-      
-    if (x == endMarker) {
-      readInProgress = false;
-      newDataFromPC = true;
-      inputBuffer[bytesRecvd] = 0;
-      parseData();
-    }
-    
-    if(readInProgress) {
-      inputBuffer[bytesRecvd] = x;
-      bytesRecvd ++;
-      if (bytesRecvd == buffSize) {
-        bytesRecvd = buffSize - 1;
-      }
-    }
-
-    if (x == startMarker) { 
-      bytesRecvd = 0; 
-      readInProgress = true;
+void measure(){
+  if (readFlag == 1){
+    sensor.takeMeasurementsWithBulb(); //This is a hard wait while all 18 channels are measured
+    Serial.print(sensor.getCalibratedA());
+    Serial.print(",");
+    Serial.print(sensor.getCalibratedB());
+    Serial.print(",");
+    Serial.print(sensor.getCalibratedC());
+    Serial.print(",");
+    Serial.print(sensor.getCalibratedD());
+    Serial.print(",");
+    Serial.print(sensor.getCalibratedE());
+    Serial.print(",");
+    Serial.print(sensor.getCalibratedF());
+    Serial.print(",");
+    Serial.print(sensor.getCalibratedG());
+    Serial.print(",");
+    Serial.print(sensor.getCalibratedH());
+    Serial.print(",");
+    Serial.print(sensor.getCalibratedI());
+    Serial.print(",");
+    Serial.print(sensor.getCalibratedJ());
+    Serial.print(",");
+    Serial.print(sensor.getCalibratedK());
+    Serial.print(",");
+    Serial.print(sensor.getCalibratedL());
+    Serial.print(",");
+    Serial.print(sensor.getCalibratedR());
+    Serial.print(",");
+    Serial.print(sensor.getCalibratedS());
+    Serial.print(",");
+    Serial.print(sensor.getCalibratedT());
+    Serial.print(",");
+    Serial.print(sensor.getCalibratedU());
+    Serial.print(",");
+    Serial.print(sensor.getCalibratedV());
+    Serial.print(",");
+    Serial.print(sensor.getCalibratedW());
+    Serial.print(",");
+    Serial.println();
+    readFlag = 0;
     }
   }
-}
 
-//=============
+
  
-void parseData() {
-
-    // split the data into its parts
-    
-  char * strtokIndx; // this is used by strtok() as an index
-  
-  strtokIndx = strtok(inputBuffer,",");      // get the first part - the string
-  strcpy(messageFromPC, strtokIndx); // copy it to messageFromPC
-  
-  strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
-  newFlashInterval = atoi(strtokIndx);     // convert this part to an integer
-  
-  strtokIndx = strtok(NULL, ","); 
-  servoFraction = atof(strtokIndx);     // convert this part to a float
-
+void loop() {
+  initiator(1);
+  measure();
 }
 
-//=============
-
-void replyToPC() {
-
-  if (newDataFromPC) {
-    newDataFromPC = false;
-    Serial.print("<Msg ");
-    Serial.print(messageFromPC);
-    Serial.print(" NewFlash ");
-    Serial.print(newFlashInterval);
-    Serial.print(" SrvFrac ");
-    Serial.print(servoFraction);
-    Serial.print(" SrvPos ");
-    Serial.print(newServoPos);
-    Serial.print(" Time ");
-    Serial.print(curMillis >> 9); // divide by 512 is approx = half-seconds
-    Serial.println(">");
-  }
-}
-
-//============
-
-void updateFlashInterval() {
-
-   // this illustrates using different inputs to call different functions
-  if (strcmp(messageFromPC, "LED1") == 0) {
-     updateLED1();
-  }
-  
-  if (strcmp(messageFromPC, "LED2") == 0) {
-     updateLED2();
-  }
-}
-
-//=============
-
-void updateLED1() {
-
-  if (newFlashInterval > 100) {
-    LEDinterval[0] = newFlashInterval;
-  }
-}
-
-//=============
-
-void updateLED2() {
-
-  if (newFlashInterval > 100) {
-    LEDinterval[1] = newFlashInterval;
-  }
-}
-
-//=============
-
-void flashLEDs() {
-
-  for (byte n = 0; n < numLEDs; n++) {
-    if (curMillis - prevLEDmillis[n] >= LEDinterval[n]) {
-       prevLEDmillis[n] += LEDinterval[n];
-       digitalWrite( ledPin[n], ! digitalRead( ledPin[n]) );
-    }
-  }
-}
-
-//=============
-
-void updateServoPos() {
-
-  byte servoRange = servoMax - servoMin;
-  if (servoFraction >= 0 && servoFraction <= 1) {
-    newServoPos = servoMin + ((float) servoRange * servoFraction);
-  }
-}
-
-//=============
-
-void moveServo() {
-  if (servoPos != newServoPos) {
-    servoPos = newServoPos;
-    myServo.write(servoPos);
-  }
-}
